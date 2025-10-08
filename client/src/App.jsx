54b43e735dee7bf9fc123b30d3fbc73237426005
sainirahul8967 <sainirahul8967@gmail.com>
2025-10-08 17:16:01 +0530

    welcome to the podosphere website

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline text-red-500">
+        Welcome to the Podosphere website!
+      </h1>
+    </>
+  );
+}
+
+export default App;
